f17db328bca11fa2f8d6a8da046c7966072bf3e3
Hung Jin Lin <gae.m.project@gmail.com>
2017-01-25 02:05:33 +0800

    build man stage interface

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+import argparse
+
 from datasets.mnist import MNist
 from models.network import CNet
 
-mnist = MNist()
-train_set, test_set = mnist.load()
-train = (train_set.x, train_set.y)
-test = (test_set.x, test_set.y)
 
-TRAIN = 0
-EVALUATE = 1
+def main(args):
 
-cnet = CNet(train=True, image_shape=mnist.image_size)
-model = cnet.model
+    mnist = MNist()
+    train_set, test_set = mnist.load()
+    train = (train_set.x, train_set.y)
+    test = (test_set.x, test_set.y)
 
-if TRAIN:
+    cnet = CNet(train=True, image_shape=mnist.image_size)
+    model = cnet.model
 
-    model.fit(*train, validation_data=test, nb_epoch=1, batch_size=512)
-    cnet.save()
+    if args.mode == 'train':
 
-elif EVALUATE:
+        model.fit(*train, validation_data=test, nb_epoch=1, batch_size=512)
+        cnet.save()
 
-    cnet.load()
-    _, accuracy = model.evaluate(*test, batch_size=512)
-    print('== %s ==\nTest accuracy: %.2f%%' % (cnet.NAME, accuracy * 100))
+    elif args.mode == 'eval':
 
-else:
+        cnet.load()
+        _, accuracy = model.evaluate(*test, batch_size=512)
+        print('== %s ==\nTest accuracy: %.2f%%' % (cnet.NAME, accuracy * 100))
 
-    cnet.load()
-    print(model.predict(test[0]))
+    else:
+
+        cnet = CNet(image_shape=mnist.image_size)
+        cnet.load()
+        print(cnet.model.predict(test[0]))
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description='Play with models')
+    p.add_argument('mode', action="store")
+    args = p.parse_args()
+
+    main(args)
