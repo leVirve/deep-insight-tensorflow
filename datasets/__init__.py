b13189271203e2fbe807aec0b2fad3e01b976f8d
Hung Jin Lin <gae.m.project@gmail.com>
2017-01-31 01:33:42 +0800

    hide the dataset and open interface

---
+++ datasets/__init__.py
@@ -0,0 +1,18 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+class MNist:
+
+    path = 'data/mnist-data'
+
+    def __init__(self, batch_size, *args, **kwargs):
+        self.raw = input_data.read_data_sets(self.path, one_hot=True, *args, **kwargs)
+        self.batch_size = batch_size
+        self.num_train_batch = self.raw.train.num_examples // batch_size
+
+    def next_batch(self):
+        return self.raw.train.next_batch(self.batch_size)
+
+    @property
+    def test_set(self):
+        return self.raw.test
--- tf_main.py
+++ tf_main.py
@@ -1,15 +1,14 @@
 import time
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 
 from models.network import TFCNN
+from datasets import MNist
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-mnist = input_data.read_data_sets('data/mnist-data', one_hot=True)
 train_dir = './logs/train'
 epochs, batch_size = 20, 1024
-total_batch = mnist.train.num_examples // batch_size
+dataset = MNist(batch_size=batch_size)
 
 with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, [None, 28 * 28])
@@ -27,17 +26,16 @@ with tf.Session() as sess:
 
     s = time.time()
     for epoch in range(epochs):
-        avg_loss = 0.
-        for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size)
+        loss = 0.
+        for i in range(dataset.num_train_batch):
+            batch_x, batch_y = dataset.next_batch()
             summary, _, c = sess.run([merged, net.optimize, net.loss], feed_dict={x: batch_x, y: batch_y})
-            avg_loss += c / total_batch
+            loss += c
         train_writer.add_summary(summary, epoch)
-        print('Epoch {:02d}: cost = {:.9f}'.format(epoch + 1, avg_loss))
+        print('Epoch {:02d}: loss = {:.9f}'.format(epoch + 1, loss / dataset.num_train_batch))
     print('Elasped time:', time.time() - s)
 
-    acc = sess.run(net.accuracy, feed_dict={x: mnist.test.images,
-                                            y: mnist.test.labels})
+    acc = sess.run(net.accuracy, feed_dict={x: dataset.test_set.images, y: dataset.test_set.labels})
     print('Testing Accuracy: {:.2f}%'.format(acc * 100))
 
     saver.save(sess, train_dir, global_step=step)
