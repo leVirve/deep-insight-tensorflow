bf5a4286309d3f89559a88d85e6d8f252a2c2b1e
Hung Jin Lin <gae.m.project@gmail.com>
2017-01-31 01:14:34 +0800

    make full example update to new interface

--- models/network.py
+++ models/network.py
@@ -1,3 +1,5 @@
+import functools
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Convolution2D, MaxPooling2D
 import tensorflow as tf
@@ -6,20 +8,29 @@ from tensorflow.python.layers import layers
 from models.base import BaseNet
 
 
+def define_scope(f, *args, **kwargs):
+
+    attr = '_cached_' + f.__name__
+
+    @property
+    @functools.wraps(f)
+    def decorator(self):
+        if not hasattr(self, attr):
+            with tf.name_scope(f.__name__, *args, **kwargs):
+                setattr(self, attr, f(self))
+        return getattr(self, attr)
+    return decorator
+
+
 class KerasCNN(BaseNet):
 
     NAME = 'KerasCNN'
 
     def build_model(self):
         layers = [
-            Convolution2D(
-                32, *(5, 5),
-                border_mode='same',
-                activation='relu',
-                input_shape=self.input_shape),
+            Convolution2D(32, *(5, 5), border_mode='same', activation='relu', input_shape=self.input_shape),
             MaxPooling2D(pool_size=(2, 2)),
-            Convolution2D(
-                64, *(5, 5), border_mode='same', activation='relu'),
+            Convolution2D(64, *(5, 5), border_mode='same', activation='relu'),
             MaxPooling2D(pool_size=(2, 2)),
             Flatten(),
             Dense(1024, activation='relu'),
@@ -41,27 +52,41 @@ class TFCNN:
 
     NAME = 'TFCNN'
 
-    def __init__(self):
-        self.model = None
+    def __init__(self, images, labels, step):
+        self.images = images
+        self.labels = labels
+        self.step = step
+        self.logits = self.build_model()
+        self.loss
+        self.optimize
+        self.accuracy
 
-    def build_model(self, x, is_train=True):
-        x = tf.reshape(x, shape=[-1, 28, 28, 1])
-        conv1 = layers.conv2d(
-            x, 32, [5, 5], padding='same', activation=tf.nn.relu, name='conv1')
-        pool1 = layers.max_pooling2d(
-            conv1, pool_size=[2, 2], strides=2, name='pool1')
-        conv2 = layers.conv2d(
-            pool1,
-            64, [5, 5],
-            padding='same',
-            activation=tf.nn.relu,
-            name='conv2')
-        pool2 = layers.max_pooling2d(
-            conv2, pool_size=[2, 2], strides=2, name='pool2')
+    def build_model(self, is_train=True):
+        x = tf.reshape(self.images, shape=[-1, 28, 28, 1])
+        conv1 = layers.conv2d(x, 32, [5, 5], padding='same', activation=tf.nn.relu, name='conv1')
+        pool1 = layers.max_pooling2d(conv1, pool_size=[2, 2], strides=2, name='pool1')
+        conv2 = layers.conv2d(pool1, 64, [5, 5], padding='same', activation=tf.nn.relu, name='conv2')
+        pool2 = layers.max_pooling2d(conv2, pool_size=[2, 2], strides=2, name='pool2')
         flat1 = tf.reshape(pool2, [-1, 7 * 7 * 64], name='flatten')
-        dense = layers.dense(
-            flat1, units=1024, activation=tf.nn.relu, name='fc1')
-        dropout = layers.dropout(
-            dense, rate=0.4, training=is_train, name='dropout')
+        dense = layers.dense(flat1, units=1024, activation=tf.nn.relu, name='fc1')
+        dropout = layers.dropout(dense, rate=0.4, training=is_train, name='dropout')
         logits = layers.dense(dropout, units=10, name='fc2')
         return logits
+
+    @define_scope
+    def loss(self):
+        xentropy = tf.losses.softmax_cross_entropy(logits=self.logits, onehot_labels=self.labels)
+        loss = tf.reduce_mean(xentropy, name='loss')
+        tf.summary.scalar('loss', loss)
+        return loss
+
+    @define_scope
+    def optimize(self):
+        return tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss, global_step=self.step)
+
+    @define_scope
+    def accuracy(self):
+        correct_pred = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
+        return accuracy
--- tf_main.py
+++ tf_main.py
@@ -4,7 +4,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 from models.network import TFCNN
 
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+tf.logging.set_verbosity(tf.logging.INFO)
+
+mnist = input_data.read_data_sets('data/mnist-data', one_hot=True)
 train_dir = './logs/train'
 epochs, batch_size = 20, 1024
 total_batch = mnist.train.num_examples // batch_size
@@ -13,20 +15,8 @@ with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, [None, 28 * 28])
     y = tf.placeholder(tf.float32, [None, 10])
 
-logits = TFCNN().build_model(x)
-
-with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y), name='loss')
-    tf.summary.scalar('loss', loss)
-
-with tf.name_scope('train'):
-    step = tf.Variable(0, name='global_step', trainable=False)
-    train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss, global_step=step)
-
-with tf.name_scope('accuracy'):
-    correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-    tf.summary.scalar('accuracy', accuracy)
+step = tf.Variable(0, name='global_step', trainable=False)
+net = TFCNN(x, y, step)
 
 merged = tf.summary.merge_all()
 
@@ -40,14 +30,14 @@ with tf.Session() as sess:
         avg_loss = 0.
         for i in range(total_batch):
             batch_x, batch_y = mnist.train.next_batch(batch_size)
-            summary, _, c = sess.run([merged, train_op, loss], feed_dict={x: batch_x, y: batch_y})
+            summary, _, c = sess.run([merged, net.optimize, net.loss], feed_dict={x: batch_x, y: batch_y})
             avg_loss += c / total_batch
         train_writer.add_summary(summary, epoch)
         print('Epoch {:02d}: cost = {:.9f}'.format(epoch + 1, avg_loss))
     print('Elasped time:', time.time() - s)
 
-    acc = sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                        y: mnist.test.labels})
+    acc = sess.run(net.accuracy, feed_dict={x: mnist.test.images,
+                                            y: mnist.test.labels})
     print('Testing Accuracy: {:.2f}%'.format(acc * 100))
 
     saver.save(sess, train_dir, global_step=step)
